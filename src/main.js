8071cbf26c5d37b5b87ea04359522c3c22e93a82
Anton Src <anton.source@gmail.com>
2023-11-21 22:15:42 +0700

    change material 2

--- src/main.js
+++ src/main.js
@@ -1,26 +1,54 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function main() {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	const canvas = document.querySelector( '#canv' );
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	const fov = 75;
+	const aspect = 2; // the canvas default
+	const near = 0.1;
+	const far = 5;
+	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.z = 2;
 
-camera.position.z = 5;
+	const scene = new THREE.Scene();
 
-function animate() {
-	requestAnimationFrame( animate );
+	{
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight( color, intensity );
+		light.position.set( - 1, 2, 4 );
+		scene.add( light );
 
-	renderer.render( scene, camera );
+	}
+
+	const boxWidth = 1;
+	const boxHeight = 1;
+	const boxDepth = 1;
+	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+
+	const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } ); // greenish blue
+
+	const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	function render( time ) {
+
+		time *= 0.001; // convert time to seconds
+
+		cube.rotation.x = time;
+		cube.rotation.y = time;
+
+		renderer.render( scene, camera );
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
+
 }
 
-animate();
+main();
