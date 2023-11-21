456965060b1f23968a127c9bf767197019016a0b
Anton Src <anton.source@gmail.com>
2023-11-21 22:04:36 +0700

    test cube 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  }
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 1048576,
+    }
 };
